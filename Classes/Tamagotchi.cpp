113bc9de7455d30620fa2530fb9b2ac762b125f6
Bak <Bak@Bakui-MacBook-Air.local>
2014-02-27 22:21:46 +0900

    Initial Commit

---
+++ Classes/Tamagotchi.cpp
@@ -0,0 +1,16 @@
+//
+//  Tamagotchi.cpp
+//  Tamagotchi
+//
+//  Created by Bak on 2014. 2. 27..
+//
+//
+
+#include "Tamagotchi.h"
+
+using namespace std;
+USING_NS_CC;
+
+void Tamagotchi::test() {
+    
+}
---
+++ Classes/Tamagotchi.h
@@ -0,0 +1,22 @@
+//
+//  Tamagotchi.h
+//  Tamagotchi
+//
+//  Created by Bak on 2014. 2. 27..
+//
+//
+
+#ifndef __Tamagotchi__Tamagotchi__
+#define __Tamagotchi__Tamagotchi__
+
+#include <iostream>
+#include "cocos2d.h"
+
+class Tamagotchi {
+    int value;
+    
+public:
+    void test();
+};
+
+#endif /* defined(__Tamagotchi__Tamagotchi__) */
